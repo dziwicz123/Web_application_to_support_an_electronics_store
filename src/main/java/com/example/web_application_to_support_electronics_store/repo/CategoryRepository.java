b5455f760c0acd3aa7ea862168306cb226a24999
dziwicz123 <97152349+dziwicz123@users.noreply.github.com>
2024-12-29 22:12:04 +0100

    addition of editing product in admin

--- src/main/java/com/example/web_application_to_support_electronics_store/repo/CategoryRepository.java
+++ src/main/java/com/example/web_application_to_support_electronics_store/repo/CategoryRepository.java
@@ -2,6 +2,12 @@ package com.example.web_application_to_support_electronics_store.repo;
 
 import com.example.web_application_to_support_electronics_store.config.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+    @Query("SELECT c FROM Category c WHERE c.CategoryName = :CategoryName")
+    Optional<Category> findByCategoryName(@Param("CategoryName") String CategoryName);
 }
